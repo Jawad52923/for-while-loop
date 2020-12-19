b008c731132ce6e8bea0a0ade24d95d492ffdaa3
Your Name <you@example.com>
2020-12-19 19:57:36 +0500

    first commit

---
+++ for+while loop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace for_while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a=Convert.ToInt16(Console.ReadLine());
+
+            int i = 1;
+            do
+            {
+                Console.WriteLine(a +"x"+ i + "=" + a*i);
+                i++;
+            } while (i<=10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+
+            //    Console.WriteLine();
+            //    i++;
+            //}
+        }
+    }
+}
